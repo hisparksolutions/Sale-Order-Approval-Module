cc552f7744854cb7b36490094d4b7625b8d734ee
M Sharjeel Shaikh <ms.servicesofficial@gmail.com>
2025-05-08 01:22:36 +0500

    update

---
+++ sales_order_approval/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Order Approval',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': """ "Sales Order Approval" is a process that requires two separate approvals for a sales order, ensuring accuracy, compliance, and minimizing the risk of errors and fraud. """,
+    'description': """ "Sales Order Approval" is a process where a sales order must be reviewed and approved by two separate individuals or departments before finalization. This procedure is designed to ensure accuracy, compliance, and minimize the risk of errors and fraud in sales transactions.""",
+    'author': 'Hi Spark Solutions',
+    'company': 'Hi Spark Solutions',
+    'maintainer': 'Hi Spark Solutions',
+    'website': 'https://www.hisparksolutions.com/',
+    'depends': ['base', 'sale_management'],
+    'data': [
+        'views/res_company_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/sale_order_views.xml'
+    ],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    "price": "10",
+    "currency": "USD",
+}
---
+++ sales_order_approval/models/sale_order.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    state = fields.Selection(selection_add=
+                             [('to_approve', 'To Approve'),
+                              ('sent',)], ondelete={'to_approve': 'cascade'})
+
+    def button_approve(self):
+        """
+            Method to approve the sale order and change its state to 'Sale'.
+        """
+        self.write({'state': 'sale'})
+
+    def action_confirm(self):
+        """
+            Override to implement double validation logic based on company settings.
+            Confirms the sale order if conditions are met; otherwise, sets the state to 'to_approve'.
+        """
+        res = super(SaleOrder, self).action_confirm()
+        if self.company_id.so_double_validation:
+            if self.env['ir.config_parameter'].sudo().get_param(
+                    'sales_order_approval.so_approval'):
+                if self.amount_total > float(
+                        self.env['ir.config_parameter'].sudo().get_param(
+                            'sales_order_approval.so_min_amount')):
+                    if self.env.user.has_group('sales_team.group_sale_manager'):
+                        self.state = 'sale'
+                    else:
+                        self.state = 'to_approve'
+        return res
+
+    def action_cancel(self):
+        """
+            Method to cancel the sale order and update its state to 'cancel'.
+        """
+        self.state = 'cancel'
